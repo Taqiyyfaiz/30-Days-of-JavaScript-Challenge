0a8d64e8cb843415c0b292095e0ef9bdc923ce8d
Taqiyyfaiz <taqiyyfaizm@gmail.com>
2024-07-16 10:06:01 +0530

    Day 1

---
+++ D01.js
@@ -0,0 +1,44 @@
+// Day 1: Variables & DataTypes
+
+// Activity 1: Variables Declaration
+// Task 1: 
+var Target = 207;
+console.log(Target)
+
+// Task 2:
+let userName = 'Taqiyyfaiz';
+console.log(userName)
+
+// Activity 2:
+// Task 3:
+const emailID = "abc@gmail.com";
+console.log(emailID)
+
+// Activity 3:
+// Task 4:
+const Year = 2000;
+const Name = "Taqiyy Faiz";
+const boolVal = true;
+const object = {
+    user:"Taqiyy Faiz",
+    accountID:"abc@xyzcompany.in",
+    Password:"2356@341"
+}
+const myArr = [0, 1, 2, 3, 4];
+
+console.log(`type of Year: ${typeof (Year)}
+            type of Name: ${typeof (Name)}
+            type of boolVal: ${typeof (boolVal)}
+            type of object: ${typeof (object)}
+            typr of myArr: ${typeof (myArr)}
+`);
+
+// Activity 4:
+// Task 5:
+let Location = "Chennai";
+Location = "Vaniyambadi";
+console.log(Location);
+
+// Activity 5:
+// Task 6:
+const 
